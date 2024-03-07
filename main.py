8c0185dc47f636da0bf89cf6b7c1a224acc74722
tathagatamaiti <tathagata.p.maiti@gmail.com>
2024-03-07 16:03:41 +0530

    added additional functionalities for initialization of the simulator

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from event import Event
 from ue import UE
 from compute_node import ComputeNode
@@ -5,15 +7,24 @@ from scheduler import Scheduler
 
 
 def main():
-    scheduler = Scheduler(total_simulation_time=20)
-    ue_list = [UE(f"UE{i}") for i in range(3)]
+    random.seed(42)
+    total_simulation_time = 30
+    num_ue = 3
+    scheduler = Scheduler(total_simulation_time)
+    ue_list = [UE(f"UE{i}") for i in range(num_ue)]
     compute_node = ComputeNode("CN0", scheduler, 0, 0, 0)
+    total_pdus_generated = 0
+
     scheduler.ue_list = ue_list
     scheduler.compute_node = compute_node
 
-    initial_events = [Event(0, 'UE_init') for _ in range(len(ue_list))]
-    for event in initial_events:
-        scheduler.schedule_event(event)
+    for ue in ue_list:
+
+        ue.generate_pdu_session()
+        pdu_generation_time = random.randint(1, total_simulation_time)
+        pdu_generation_event = Event(pdu_generation_time, 'UE_init', ue)
+        scheduler.schedule_event(pdu_generation_event)
+        total_pdus_generated += 1
 
     scheduler.run_simulation()
 
